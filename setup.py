6b9c76bbf6172375fb99ba8905f5b6092dd933f3
isabelontheway <isabelguan1992@gmail.com>
2018-02-04 14:36:27 +0800

    add test in setup

--- setup.py
+++ setup.py
@@ -6,5 +6,11 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-    ]
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest'
+    ],
 )
